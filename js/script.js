842a989c08df4e40569bf8fa6e34addc25a062f9
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-10 17:22:04 +0100

    Add: class per layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //richiamo il container da riempire
-const rowHtml = document.querySelector('.row');
+const rowHtml = document.querySelector('.row-custom');
 
 
 //ciclo i numeri da 1 a 100
@@ -13,24 +13,26 @@ for(let i=1; i<101; i++){
         const divHtml=document.createElement('div');
         divHtml.innerText="fizzbuzz";
         rowHtml.append(divHtml);
+        divHtml.classList.add("box-custom", "text-center")
 
         
     }else if(i % 5===0){
         const divHtml=document.createElement('div');
         divHtml.innerText="buzz";
         rowHtml.append(divHtml);
-        
+        divHtml.classList.add("box-custom", "text-center")
         
     }else if(i % 3===0){
         const divHtml=document.createElement('div');
         divHtml.innerText="fizz";
         rowHtml.append(divHtml);
-        
-
+        divHtml.classList.add("box-custom", "text-center")
         
     }else{
         const divHtml=document.createElement('div');
         divHtml.innerText=`${i}`;
         rowHtml.append(divHtml);
+        divHtml.classList.add("box-custom", "text-center")
+
     }
 }
