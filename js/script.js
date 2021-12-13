032992e0b4c5b76d76acf1d9642ab91820fd7c99
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-13 10:29:22 +0100

    Optimize: js code

--- js/script.js
+++ js/script.js
@@ -5,33 +5,27 @@ const rowHtml = document.querySelector('.row-custom');
 //ciclo i numeri da 1 a 100
 for(let i=1; i<101; i++){
 
+    const divHtml=document.createElement('div');
+    rowHtml.append(divHtml);
+
     //se il numero è divisibile per 5 e per 3 stampo fizzbuzz
     //altrimenti se il numero è divisibile solo per 5 stampo buzz
     //altrimenti se il numero è divisibile solo per 3 stampo fizz
     //altrimenti stampo il numero
     if(i % 5===0 && i % 3===0){
-        const divHtml=document.createElement('div');
         divHtml.innerText="fizzbuzz";
-        rowHtml.append(divHtml);
         divHtml.classList.add("box-custom", "text-center", "fw-bold", "fizzbuzz-color")
-
         
     }else if(i % 5===0){
-        const divHtml=document.createElement('div');
         divHtml.innerText="buzz";
-        rowHtml.append(divHtml);
         divHtml.classList.add("box-custom", "text-center", "fw-bold", "buzz-color")
         
     }else if(i % 3===0){
-        const divHtml=document.createElement('div');
         divHtml.innerText="fizz";
-        rowHtml.append(divHtml);
         divHtml.classList.add("box-custom", "text-center", "fw-bold", "fizz-color")
         
     }else{
-        const divHtml=document.createElement('div');
         divHtml.innerText=`${i}`;
-        rowHtml.append(divHtml);
         divHtml.classList.add("box-custom", "text-center", "fw-bold", "standard-color")
 
     }
